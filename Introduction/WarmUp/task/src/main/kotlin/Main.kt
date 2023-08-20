371d7e4d90c55da2265125a2522510ef4cfc9ec3
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-08-20 12:38:33 +0200

    warm up 9/10 done

--- Introduction/WarmUp/task/src/main/kotlin/Main.kt
+++ Introduction/WarmUp/task/src/main/kotlin/Main.kt
@@ -1,18 +1,18 @@
 fun main() {
-    println(getGameRules(4, 3, "ACEB"))
-    playGame(generateSecret(), 4, 3)
+    println(getGameRules(4, 3, "ACEB", "ABCDEFGH"))
+    playGame(generateSecret(4, "ABCDEFGH"), 4, 3)
 }
 
-fun getGameRules(wordLength: Int, maxAttemptsCount: Int, secretExample: String): String {
+fun getGameRules(wordLength: Int, maxAttemptsCount: Int, secretExample: String, alphabet: String): String {
     return """Welcome to the game! 
 
-Two people play this game: one chooses a word (a sequence of letters), the other guesses it. In this version, the computer chooses the word: a sequence of $wordLength letters (for example, $secretExample). The user has several attempts to guess it (the max number is $maxAttemptsCount). For each attempt, the number of complete matches (letter and position) and partial matches (letter only) is reported.
+Two people play this game: one chooses a word (a sequence of letters), the other guesses it. In this version, the computer chooses the word: a sequence of $wordLength letters (for example, $secretExample). The user has several attempts to guess it (the max number is $maxAttemptsCount). For each attempt, the number of complete matches (letter and position) and partial matches (letter only) is reported.  The possible symbols in the word: $alphabet. 
 
 For example, with $secretExample as the hidden word, the BCDF guess will give 1 full match (C) and 1 partial match (B)."""
 }
 
-fun generateSecret(): String {
-    return "ABCD"
+fun generateSecret(wordLength:Int, alphabet: String): String {
+    return List(wordLength) {alphabet.random()}.joinToString("")
 }
 
 fun playGame(secret: String, wordLength: Int, maxAttemptsCount: Int) {
