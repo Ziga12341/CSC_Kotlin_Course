7745fec14a54cfdca7cab657f8224ea76c40a697
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-08-21 14:07:41 +0200

    Task 3: Warm up: 10/10 - fix problems

--- Introduction/WarmUp/task/src/main/kotlin/Main.kt
+++ Introduction/WarmUp/task/src/main/kotlin/Main.kt
@@ -1,31 +1,30 @@
 fun main() {
     println(getGameRules(4, 3, "ACEB", "ABCDEFGH"))
-    playGame(generateSecret(4, "ABCDEFGH"), 4, 3, "ABCDEFGH")
+    playGame(generateSecret(4, "ABCDEFGH"), 4, 3)
 }
 
 fun getGameRules(wordLength: Int, maxAttemptsCount: Int, secretExample: String, alphabet: String): String {
     return """Welcome to the game! 
 
 Two people play this game: one chooses a word (a sequence of letters), the other guesses it. In this version, the computer chooses the word: a sequence of $wordLength letters (for example, $secretExample). The user has several attempts to guess it (the max number is $maxAttemptsCount). For each attempt, the number of complete matches (letter and position) and partial matches (letter only) is reported.  The possible symbols in the word: $alphabet. 
-    
+
 For example, with $secretExample as the hidden word, the BCDF guess will give 1 full match (C) and 1 partial match (B)."""
 }
+
 fun generateSecret(wordLength:Int, alphabet: String): String {
     return List(wordLength) {alphabet.random()}.joinToString("")
 }
 
-fun playGame(secret: String, wordLength: Int, maxAttemptsCount: Int, alphabet: String) {
+fun playGame(secret: String, wordLength: Int, maxAttemptsCount: Int) {
     var attemptsCounter = 0
-    var guess = ""
+    var guess: String
     do {
-        guess = safeUserInput(wordLength, guess)
-        if (!isCorrectInput(guess, wordLength, alphabet)){
-            break
-        }
+        println("Please input your guess. It should be of length $wordLength.")
+        guess = safeReadLine()
         attemptsCounter++
         if (isWin(isComplete(secret, guess), attemptsCounter, attemptsCounter)) {
             printRoundResults(secret, guess)
-            println("Congratulations! You guessed it!")
+            println("Congratulations!   You guessed it!")
             break
         }
         printRoundResults(secret, guess)
@@ -33,25 +32,6 @@ fun playGame(secret: String, wordLength: Int, maxAttemptsCount: Int, alphabet: S
             println("Sorry, you lost! :( My word is $secret")
         }
     } while (attemptsCounter < maxAttemptsCount + 1)
-}
-
-fun safeUserInput(wordLength: Int, alphabet: String): String {
-    println("Please input your guess. It should be of length $wordLength, and each symbol should be from the alphabet: $alphabet.")
-    return safeReadLine()
-}
-
-fun isCorrectInput(userInput:String, wordLength:Int, alphabet:String):Boolean{
-    if (userInput.length != wordLength){
-        println("The length of your guess should be $wordLength characters! Try again!")
-        return false
-    }
-    for (char in userInput){
-        if (!alphabet.contains(char)){
-            println("All symbols in your guess should be the $alphabet alphabet characters! Try again!")
-            return false
-        }
-    }
-    return true
 }
 
 fun isWin(complete: Boolean, attempts: Int, maxAttemptsCount: Int): Boolean {
@@ -106,6 +86,7 @@ fun countPartialMatches(secret: String, guess: String): Int {
     return counter
 }
 
+//why type Char do not work for letter "a" or "A"... i need to put type String here
 //This function is case sensitive!!!
 fun charInWord(letter: Char, word: String): Boolean {
     return letter in word
