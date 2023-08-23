6425725b7b1733ecb5f38f70d077e474f33a89e8
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-08-23 11:14:30 +0200

    Task 5: AlmostDone: 3/8 done

--- Introduction/AlmostDone/task/src/main/kotlin/Main.kt
+++ Introduction/AlmostDone/task/src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
     // Write your solution in this file
 }
+
+fun trimPicture(picture: String): String {
+    return picture.trimIndent()
+}
+
+fun applyFilter(trimmedPicture: String, filterName: String): String {
+    return when (filterName) {
+        "applyBordersFilter" -> applyBordersFilter(trimmedPicture)
+        "applySquaredFilter" -> applySquaredFilter(trimmedPicture)
+        else -> "This filter not implemented yet"
+    }
+}
+
+fun applyBordersFilter(picture: String): String {
+    TODO()
+}
+
+fun applySquaredFilter(picture: String): String {
+    TODO()
+}
