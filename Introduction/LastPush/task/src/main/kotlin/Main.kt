22e64b9dd4a27726fa4846cd9b19fe643d193c6d
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-08-26 17:37:19 +0200

    Task 6: lastPush: 2/5 done

--- Introduction/LastPush/task/src/main/kotlin/Main.kt
+++ Introduction/LastPush/task/src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    // Write your code here
+    println(fillPatternRow("|-|", 6))
+    println(getPatternHeight("123\n123\n123"))
+
+}
+
+fun fillPatternRow(patternRow:String, patternWidth:Int):String{
+//    add separator to row
+//    Please, throw an IllegalStateException if patternRow.length > patternWidth.
+    if (patternRow.length > patternWidth) throw IllegalStateException() else {
+        return patternRow + separator.toString().repeat(patternWidth - patternRow.length)
+    }
+}
+
+fun getPatternHeight(pattern: String):Int{
+//    calculates pattern height
+//    convert pattern into list line by line ang get its size
+    return pattern.lines().size
 }
